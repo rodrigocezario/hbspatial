f0d1f2c8eacb38feeeb35ca27afd71352e729202
rodrigocezario <rodrigocezario@msn.com>
2021-01-26 19:54:43 -0300

    init commmit

---
+++ hbspatial/src/main/java/org/cezario/HbspatialApplication.java
@@ -0,0 +1,13 @@
+package org.cezario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbspatialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbspatialApplication.class, args);
+	}
+
+}
